ef2c6f0c5f3b4589766c84cb1f18011850b53168
loadystudio <loadycode@outlook.com>
2022-03-03 00:08:27 +0300

    updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('swtb.js');
 toolbox.precache(['index.html','style.css']);
-toolbox.router.get('/img/*',toolbox.cacheFirst);
-toolbox.router.get('/*',toolbox.networkFirst,{networkTimeoutSeconds:5});
+toolbox.router.get('img/*',toolbox.cacheFirst);
+toolbox.router.get('*',toolbox.networkFirst,{networkTimeoutSeconds:5});
