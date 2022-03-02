04c3033942993c9f7e342065dcfbbe4316119ac3
loadystudio <loadycode@outlook.com>
2022-03-03 00:41:26 +0300

    updated

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 date=new Date();hour=date.getHours();theme='light';
 function theme_set(i){
 	if(i=='dark'){
-		$('body').css('background','black');
-		$('#qr').css('background','url(img/tg-dark.png)');
-		$('#qr').css('background-size','cover');
-		$('h1,h2,h3,p,a').css('color','whitesmoke');
-		$('.gr,.b').css('color','black');
+		document.body.style.background='black';
+		document.getElementById('qr').style.background='url(img/tg-dark.png)';
+		document.getElementById('qr').style.backgroundSize='cover';
+		document.getElementsByTagName('h1')[0].style.color='whitesmoke';
+		document.getElementsByTagName('h1')[1].style.color='whitesmoke';
+		document.getElementsByTagName('h2')[0].style.color='whitesmoke';
+		document.getElementsByTagName('h2')[1].style.color='whitesmoke';
+		document.getElementsByClassName('gr')[0].style.color='black';
+		document.getElementsByClassName('b')[0].style.color='black';
 		theme='dark';
 	}else{
-		$('body').css('background','whitesmoke');
-		$('#qr').css('background','url(img/tg.png)');
-		$('#qr').css('background-size','cover');
-		$('h1,h2,h3,p,a').css('color','black');
-		$('.gr,.b').css('color','whitesmoke');
+		document.body.style.background='whitesmoke';
+		document.getElementById('qr').style.background='url(img/tg.png)';
+		document.getElementById('qr').style.backgroundSize='cover';
+		document.getElementsByTagName('h1')[0].style.color='black';
+		document.getElementsByTagName('h1')[1].style.color='black';
+		document.getElementsByTagName('h2')[0].style.color='black';
+		document.getElementsByTagName('h2')[1].style.color='black';
+		document.getElementsByClassName('gr')[0].style.color='whitesmoke';
+		document.getElementsByClassName('b')[0].style.color='whitesmoke';
 		theme='light';
 	}
 }if(hour>20||hour<10){theme_set('dark')}
 function theme_switch(){if(theme=='dark'){theme_set('light')}else{theme_set('dark')}}
+function tg(){document.location.href='https://t.me/ldstd'}
