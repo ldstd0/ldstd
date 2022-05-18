babb7234c3c4b98fd6a395b016c1956a3dcde6c2
ldstd0 <loadycode@outlook.com>
2022-05-18 19:39:04 +0300

    updated

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-date=new Date();hour=date.getHours();theme='light';
-function theme_set(i){
-	if(i=='dark'){
-		document.body.style.background='black';
-		document.getElementById('qr').style.background='url(img/tg-dark.png)';
-		document.getElementById('qr').style.backgroundSize='cover';
-		document.getElementsByTagName('h1')[0].style.color='whitesmoke';
-		document.getElementsByTagName('h1')[1].style.color='whitesmoke';
-		document.getElementsByTagName('h2')[0].style.color='whitesmoke';
-		document.getElementsByTagName('h2')[1].style.color='whitesmoke';
-		document.getElementsByClassName('gr')[0].style.color='black';
-		document.getElementsByClassName('b')[0].style.color='black';
-		theme='dark';
+date = new Date()
+hour = date.getHours()
+theme = 'light'
+function theme_set(i) {
+	if (i == 'dark') {
+		$('body').css('background','black')
+		$('#qr').css('background','url(img/tg-dark.png)')
+		$('#qr').css('background-size','cover')
+		$('h1').css('color', 'whitesmoke')
+		$('h2').css('color', 'whitesmoke')
+		$('.gr').css('color','whitesmoke')
+		$('h3').css('color','whitesmoke')
+		theme='dark'
 	}else{
-		document.body.style.background='whitesmoke';
-		document.getElementById('qr').style.background='url(img/tg.png)';
-		document.getElementById('qr').style.backgroundSize='cover';
-		document.getElementsByTagName('h1')[0].style.color='black';
-		document.getElementsByTagName('h1')[1].style.color='black';
-		document.getElementsByTagName('h2')[0].style.color='black';
-		document.getElementsByTagName('h2')[1].style.color='black';
-		document.getElementsByClassName('gr')[0].style.color='whitesmoke';
-		document.getElementsByClassName('b')[0].style.color='whitesmoke';
-		theme='light';
+		$('body').css('background','whitesmoke')
+		$('#qr').css('background','url(img/tg.png)')
+		$('#qr').css('background-size','cover')
+		$('h1').css('color', 'black')
+		$('h2').css('color', 'black')
+		$('.gr').css('color','whitesmoke')
+		$('#l').css('color', 'whitesmoke')
+		$('h3').css('color','whitesmoke')
+		theme='light'
 	}
 }if(hour>20||hour<10){theme_set('dark')}
 function theme_switch(){if(theme=='dark'){theme_set('light')}else{theme_set('dark')}}
 function tg(){document.location.href='https://t.me/ldstd'}
+function gh(){document.location.href='https://github.com/ldstd0/notty'}
